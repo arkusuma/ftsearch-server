2167975bc7ac1073f53a77694164b6904ae9d6c2
Anugrah Redja Kusuma <anugrah.redja@gmail.com>
2012-07-04 22:40:35 +0700

    Implement /api/link

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import bottle
-from bottle import request
+from bottle import request, response
 
 import sys
 import urllib2
@@ -17,15 +17,32 @@ def home():
 
 @app.route('/api/search')
 def search():
-    resp = urllib2.urlopen(
-            'http://www.filestube.com/search.html?%s' % 
-            request.query_string)
-    html = resp.read().decode('utf-8').replace('&nbsp;', ' ')
+    try:
+        response.content_type = 'application/json'
+        resp = urllib2.urlopen(
+                'http://www.filestube.com/search.html?%s' % 
+                request.query_string)
+        html = resp.read().decode('utf-8').replace('&nbsp;', ' ')
 
-    parser = SearchParser()
-    parser.feed(html)
-    result = {'total': parser.total, 'index': parser.index, 'items': parser.items}
-    return json.dumps(result)
+        parser = SearchParser()
+        parser.feed(html)
+        result = {'total': parser.total, 'index': parser.index, 'items': parser.items}
+        return json.dumps(result)
+    except:
+        return json.dump({'total': 0})
+
+@app.route('/api/link/<id:re:.*>')
+def link(id):
+    try:
+        response.content_type = 'application/json'
+        resp = urllib2.urlopen('http://www.filestube.com/%s' % id)
+        html = resp.read().decode('utf-8')
+        parser = LinkParser()
+        parser.feed(html)
+        result = [{'name': x[0], 'link': x[1]} for x in zip(parser.names, parser.links)]
+        return json.dumps(result)
+    except:
+        return json.dumps([])
 
 class SearchParser(HTMLParser):
     def __init__(self):
@@ -41,11 +58,11 @@ class SearchParser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         attrs = dict(attrs)
+        if self._text != None:
+            self._handle_text()
         if tag == 'div' and attrs.get('id') == 'newresult':
             self._item = {}
         elif self._item != None:
-            if self._text != None:
-                self._handle_text()
             self._tags.append({'tag': tag, 'attrs': attrs})
             if tag == 'a' and len(self._tags) == 1:
                 href = attrs.get('href', '')
@@ -54,26 +71,24 @@ class SearchParser(HTMLParser):
             self._in_book3 = True
         elif self._in_book3 and tag == 'span':
             self._in_span = True
-        self._text = None
 
     def handle_endtag(self, tag):
+        if self._text != None:
+            self._handle_text()
         if self._item != None:
-            if self._text != None:
-                self._handle_text()
             if len(self._tags) == 0:
                 self.items.append(self._item)
                 self._item = None
             else:
                 self._tags.pop()
         elif self._in_span:
-            self._handle_text()
             self._in_span = False
         elif self._in_book3 and tag == 'div':
             self._in_book3 = False
-        self._text = None
 
     def _handle_text(self):
         text = self._text
+        self._text = None
         if len(self._tags) == 1 and self._tags[-1]['tag'] == 'a':
             self._item['title'] = text
         elif len(self._tags) == 2 and self._tags[-1]['tag'] == 'span':
@@ -112,6 +127,27 @@ class SearchParser(HTMLParser):
         else:
             c = unichr(int(name))
         self.handle_data(c)
+
+class LinkParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.names = []
+        self.links = []
+        self._in_pre = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a' and len(attrs) > 0 and attrs[0][0] == 'title':
+            self.names.append(attrs[0][1])
+        elif tag == 'pre':
+            self._in_pre = True
+
+    def handle_endtag(self, tag):
+        if tag == 'pre':
+            self._in_pre = False
+
+    def handle_data(self, data):
+        if self._in_pre:
+            self.links = re.split('\s+', data.strip())
 
 if __name__ == '__main__':
     host = '0.0.0.0'
